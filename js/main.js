480802fbcb1bae7a9a698341803c7e3bb41718de
Sedup <69250193+Sedup@users.noreply.github.com>
2020-09-22 23:52:39 +0300

    create: add API map

--- js/main.js
+++ js/main.js
@@ -12,3 +12,15 @@ var mySwiper = new Swiper(".swiper-container", {
     enebled: true,
   },
 });
+
+
+// Initialize and add the map
+function initMap() {
+  // The location of Hotel
+  var hotel = { lat: 7.890760, lng: 98.294729 };
+  // The map, centered at Hotel
+  var map = new google.maps.Map(
+    document.getElementById('map'), { zoom: 17, center: hotel });
+  // The marker, positioned at hotel
+  var marker = new google.maps.Marker({ position: hotel, map: map });
+}
