482d90fbcc32c28b6a62d78e3fb5dfaf0a755b54
Sedup <69250193+Sedup@users.noreply.github.com>
2020-10-01 22:11:03 +0300

    create send forms (final)

--- send.php
+++ send.php
@@ -8,7 +8,10 @@ require 'phpmailer/Exception.php';
 $name = $_POST['name'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
+$newEmail = $_POST['newEmail'];
 
+
+if(!$newEmail) {
 // Формирование самого письма
 $title = "Новое обращение Best Tour Plan";
 $body = "
@@ -30,13 +33,15 @@ try {
     // Настройки вашей почты
     $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
     $mail->Username   = 'sedup11@gmail.com'; // Логин на почте
-    $mail->Password   = ''; // Пароль на почте
+    $mail->Password   = '*************'; // Пароль на почте
     $mail->SMTPSecure = 'ssl';
     $mail->Port       = 465;
     $mail->setFrom('sedup11@gmail.com', 'Сергей Дуплий'); // Адрес самой почты и имя отправителя
 
     // Получатель письма
     $mail->addAddress('sedup11@gmail.com');  
+
+    
 
 // Отправка сообщения
 $mail->isHTML(true);
@@ -46,11 +51,62 @@ $mail->Body = $body;
 // Проверяем отравленность сообщения
 if ($mail->send()) {$result = "success";} 
 else {$result = "error";}
+    
 
 } catch (Exception $e) {
     $result = "error";
     $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
 }
 
+    header('Location: thankyou.html');  
+}
+else {
+    $title = "Запрос на рассылку новостей";
+$body = "
+<h2>Новый запрос на рассылку новостей</h2>
+<b>Пользователь с адресом - </b> $newEmail подписался на рассылку новостей!<br> 
+";
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    // $mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
+    $mail->Username   = 'sedup11@gmail.com'; // Логин на почте
+    $mail->Password   = '**********'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('sedup11@gmail.com', 'Сергей Дуплий'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    $mail->addAddress('sedup11@gmail.com');  
+
+    
+
+// Отправка сообщения
+$mail->isHTML(true);
+$mail->Subject = $title;
+$mail->Body = $body;    
+
+// Проверяем отравленность сообщения
+if ($mail->send()) {$result = "success";} 
+else {$result = "error";}
+    
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+header('Location: thankyounews.html');
+}
+
 // Отображение результата
-header('Location: thankyou.html');
+    
+// echo json_encode(["result" => $result, "resultfile" => $rfile, "status" => $status]);
